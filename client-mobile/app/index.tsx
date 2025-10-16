14f514dc4a8ff2becd24775097c207260cf63fc7
Caio <caio100dcw@gmail.com>
2025-10-16 00:03:02 -0300

    feat: add background in screen

--- client-mobile/app/index.tsx
+++ client-mobile/app/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { useRouter } from "expo-router";
 import { View, Text, TouchableOpacity} from "react-native";
+import Background  from "../src/assets";
 
 const App: React.FC = () => {
   const router = useRouter();
   return (
     <View className="flex-1 bg-zinc-950">
+      <Background />
 
       <View className="absolute inset-0 items-center justify-center">
         <Text className="text-6xl font-bold text-[#FFA62B] mb-6 text-center">ShavEasy</Text>
