d0694fbb3f533c5658ec67f821a2e67a547066fc
Guilherme Chaves <guilhermechavesena3@gmail.com>
2025-12-02 23:12:21 -0300

    correção

--- client-mobile/src/services/api.ts
+++ client-mobile/src/services/api.ts
@@ -4,7 +4,7 @@ import { Platform } from 'react-native';
 const api = axios.create({
     baseURL: Platform.OS === 'web'
         ? 'http://localhost:3333'
-        : 'http://192.168.1.254:3333',
+        : '',
 });
 
 export default api;
